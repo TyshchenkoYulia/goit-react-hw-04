81c2a46b8f40f57af0d12c55a3550c1594729a88
TyshchenkoYulia <145122847+TyshchenkoYulia@users.noreply.github.com>
2024-04-18 18:06:08 +0300

    create imgCard

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ export default function App() {
       } catch (error) {
         setError(true);
       } finally {
-        setLoading(true);
+        setLoading(false);
       }
     };
     getImages();
--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,9 +1,26 @@
 import css from "./ImageCard.module.css";
 
-export default function ImageCard({ alt_description, urls: { small } }) {
+export default function ImageCard({
+  alt_description,
+  urls: { small },
+  likes,
+  views,
+  downloads,
+}) {
   return (
     <div className={css.container}>
-      <img src={small} alt={alt_description} />
+      <img className={css.img} src={small} alt={alt_description} />
+      <ul className={css.list}>
+        <li className={css.item}>
+          Likes<p className={css.text}>{likes}</p>
+        </li>
+        <li className={css.item}>
+          Views<p className={css.text}>{views}</p>
+        </li>
+        <li className={css.item}>
+          Downloads<p className={css.text}>{downloads}</p>
+        </li>
+      </ul>
     </div>
   );
 }
