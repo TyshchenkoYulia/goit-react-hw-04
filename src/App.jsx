be04ad5aff6fdc2794a59a25ed4c2973a827ee02
TyshchenkoYulia <145122847+TyshchenkoYulia@users.noreply.github.com>
2024-04-18 13:41:54 +0300

    added imgCards

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-// import { useState } from 'react'
 import SearchBar from "./components/SearchBar/SearchBar";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
+import { useState } from "react";
 // import { fetchImages } from "./image-api";
 
 export default function App() {
+  const [imgs, setImgs] = useState([]);
   const handleSubmit = () => {};
 
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
-      <ImageGallery />
+      {imgs.length > 0 && <ImageGallery items={imgs} />}
     </div>
   );
 }
